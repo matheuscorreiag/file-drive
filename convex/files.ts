084d4d9db3be74fba5777b5f9a3ff9ab40302529
Matheus Correia <matheus.correia@finderup.com.br>
2024-07-11 15:23:09 -0300

    feat: add validation to orgid

--- convex/files.ts
+++ convex/files.ts
@@ -1,12 +1,44 @@
-import { mutation, query } from "./_generated/server";
-import { v } from "convex/values";
+import { mutation, MutationCtx, query, QueryCtx } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { getUser } from "./users";
 
+async function hasAcessToOrg(
+  ctx: QueryCtx | MutationCtx,
+  tokenIdentifier: string,
+  orgId: string
+) {
+  const user = await getUser(ctx, tokenIdentifier);
+
+  const hasAccess =
+    user.orgIds.includes(orgId) || user.tokenIdentifier.includes(orgId);
+
+  if (!hasAccess) {
+    throw new ConvexError("Not authorized");
+  }
+
+  return user;
+}
 export const createFile = mutation({
   args: {
     name: v.string(),
     orgId: v.string(),
   },
   handler: async (ctx, args) => {
+    const identidy = await ctx.auth.getUserIdentity();
+
+    if (!identidy) {
+      return [];
+    }
+    const hasAccess = await hasAcessToOrg(
+      ctx,
+      identidy.tokenIdentifier,
+      args.orgId
+    );
+
+    if (!hasAccess) {
+      throw new ConvexError("Not authorized");
+    }
+
     await ctx.db.insert("files", { name: args.name, orgId: args.orgId });
   },
 });
