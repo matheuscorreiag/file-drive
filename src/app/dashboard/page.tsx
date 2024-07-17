bde747c6a94d9c088f16fa4f8c2ffa17ab769774
Matheus Correia <matheus.correia@finderup.com.br>
2024-07-17 00:46:30 -0300

    feat: add page spinner and empty illustration

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,8 @@ import { useQuery } from "convex/react";
 import { api } from "@/../convex/_generated/api";
 import { useOrganization, useUser } from "@clerk/clerk-react";
 import { FileCard } from "@/app/dashboard/components/FileCard";
+import Image from "next/image";
+import { Loader2 } from "lucide-react";
 
 export default function Dashboard() {
   const organization = useOrganization();
@@ -16,17 +18,41 @@ export default function Dashboard() {
   }
 
   const files = useQuery(api.files.getFile, orgId ? { orgId } : "skip");
+  const isLoading = files === undefined;
 
   return (
     <div className="container mx-auto pt-12">
-      <div className="flex justify-between items-center mb-8">
-        <h1 className="text-4xl font-bold">Your Files</h1>
-        <UploadButton />
-      </div>
+      {isLoading && (
+        <div className="flex flex-col gap-8 w-full items-center mt-24">
+          <Loader2 className="h-32 w-32 animate-spin text-gray-700" />
+          <div className="text-2xl">Loading your files...</div>
+        </div>
+      )}
+      {!isLoading && files.length === 0 && (
+        <div className="flex flex-col gap-4 w-full items-center mt-12">
+          <Image
+            alt="A image of a picture and a directory of files"
+            width={300}
+            height={300}
+            src="/empty.svg"
+          />
+          <div className="text-2xl"> You have no files, upload one now!</div>
+          <UploadButton />
+        </div>
+      )}
 
-      <div className="grid grid-cols-4 gap-4">
-        {files?.map((file) => <FileCard key={file._id} file={file} />)}
-      </div>
+      {!isLoading && files.length > 0 && (
+        <>
+          <div className="flex justify-between items-center mb-8">
+            <h1 className="text-4xl font-bold">Your Files</h1>
+            <UploadButton />
+          </div>
+
+          <div className="grid grid-cols-4 gap-4">
+            {files?.map((file) => <FileCard key={file._id} file={file} />)}
+          </div>
+        </>
+      )}
     </div>
   );
 }
