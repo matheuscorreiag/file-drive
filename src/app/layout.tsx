ac7a9694559dbc3e3a0d41d4ecd9b767655f9259
Matheus Correia <matheus.correia@finderup.com.br>
2024-07-25 00:03:50 -0300

    refactor: add folder to providers component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Providers from "@/components/custom/providers";
+import Providers from "@/components/custom/Providers";
 import { Toaster } from "@/components/ui/toaster";
 import { Header } from "@/components/custom/Header";
 
